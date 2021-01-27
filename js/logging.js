ec703e05eb816f7e7586501ae3ed15fec8deaa8f
Nathan Peckham <59847922+nmpeckham@users.noreply.github.com>
2021-01-26 23:25:18 -0330

    Added historical charts for temp and humidity

--- js/logging.js
+++ js/logging.js
@@ -11,7 +11,7 @@ function queryData() {
         IndexName: "device-index",
         KeyConditionExpression: "#T = :t",
         ScanIndexForward: false,
-        Limit: 1,
+        Limit: 120,
         // FilterExpression: "#T > :t",
         ExpressionAttributeNames: {"#T": "device"},
         ExpressionAttributeValues: {":t": "0"}
@@ -26,13 +26,47 @@ function queryData() {
         console.log(data.Items[0].hum)
         document.getElementById("temp").innerHTML = "Temperature" + "<br>" + data.Items[0].temp.toFixed(1) + "&degC"
         document.getElementById("hum").innerHTML = "Humidity" + "<br>" + data.Items[0].hum.toFixed(1) + "%"
-        document.getElementById("title").innerHTML = "Most Recent Reading: " + GetFormattedTime(data.Items[0].timestamp)
+        document.getElementById("title").innerHTML = "Most Recent Reading: " + getFormattedTime(data.Items[0].timestamp)
+        drawChart("tempChart", data.Items.map(a=> a.temp.toFixed(2)).reverse(), data.Items.map(a=> getFormattedTime(a.timestamp).substr(0, 8)).reverse())
+        drawChart("humChart", data.Items.map(a=> a.hum.toFixed(2)).reverse(), data.Items.map(a=> getFormattedTime(a.timestamp).substr(0, 8)).reverse())
     });
+    
 }
 
-function GetFormattedTime(timestamp) {
+function getFormattedTime(timestamp) {
     var currentDate = new Date(timestamp * 1000)
     return currentDate.getHours() + ":" + ("0" + currentDate.getMinutes()).substr(-2) + ":" + ("0" + currentDate.getSeconds()).substr(-2) + " " + currentDate.getDate() + "/" + ("0" + (currentDate.getMonth() + 1)).substr(-2) + "/" + currentDate.getFullYear()
+}
+
+function drawChart(type, readingData, timestamps) {
+    var minY = (Math.min(...readingData)).toFixed() - 1;
+    var maxY = parseFloat((Math.max(...readingData)).toFixed()) + 1;
+
+    var ctx = document.getElementById(type).getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: timestamps,
+            datasets: [{
+                label: type == "tempChart" ? "Temperature" : "Humidity",
+                data: readingData,
+                backgroundColor: [
+                    type == "tempChart" ? 'rgba(255, 99, 132, 0.4)' : 'rgba(128, 50, 255, 0.4)',
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        min: minY,
+                        max: maxY
+                    }
+                }]
+            }
+        }
+    });
 }
 
 queryData()
