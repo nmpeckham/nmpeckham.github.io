9093009bbc90205a063f58b53207f9b90ad52cef
Nathan Peckham <59847922+nmpeckham@users.noreply.github.com>
2021-01-26 20:38:19 -0330

    Fixed incorrect temp being displayed on load, added time of reading

--- js/logging.js
+++ js/logging.js
@@ -1,11 +1,7 @@
-
-
-
 AWS.config.region = 'ca-central-1'; // Region
 AWS.config.credentials = new AWS.CognitoIdentityCredentials({
     IdentityPoolId: 'ca-central-1:1453e5ba-641c-4717-a93b-0d7434d591f3',
 });
-document.getElementById("temp").innerHTML = "35.2"
 
 var docClient = new AWS.DynamoDB.DocumentClient();
 
@@ -30,7 +26,13 @@ function queryData() {
         console.log(data.Items[0].hum)
         document.getElementById("temp").innerHTML = "Temperature" + "<br>" + data.Items[0].temp.toFixed(1) + "&degC"
         document.getElementById("hum").innerHTML = "Humidity" + "<br>" + data.Items[0].hum.toFixed(1) + "%"
+        document.getElementById("title").innerHTML = "Most Recent Reading: " + GetFormattedTime(data.Items[0].timestamp)
     });
+}
+
+function GetFormattedTime(timestamp) {
+    var currentDate = new Date(timestamp * 1000)
+    return currentDate.getHours() + ":" + ("0" + currentDate.getMinutes()).substr(-2) + ":" + ("0" + currentDate.getSeconds()).substr(-2) + " " + currentDate.getDate() + "/" + ("0" + (currentDate.getMonth() + 1)).substr(-2) + "/" + currentDate.getFullYear()
 }
 
 queryData()
